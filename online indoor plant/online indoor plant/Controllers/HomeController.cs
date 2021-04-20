8d437fb06847ffa8065ea12711fa0d49e48d58a0
Rutujakauthale <rutujakauthale28@gmail.com>
2021-04-20 16:13:51 +0530

    first commit

---
+++ online indoor plant/online indoor plant/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using online_indoor_plant.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace online_indoor_plant.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Fertilizer()
+        {
+            return View();
+        }
+        public IActionResult Seeds()
+        {
+            return View();
+        }
+        public IActionResult SortBy()
+        {
+            return View();
+        }
+        public IActionResult Accessories()
+        {
+            return View();
+        }
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ online indoor plant/online indoor plant/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace online_indoor_plant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
